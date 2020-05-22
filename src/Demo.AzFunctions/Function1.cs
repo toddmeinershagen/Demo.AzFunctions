c61932ce3162594140e881de3f9b84c6350d33c5
Todd Meinershagen <todd@meinershagen.net>
2020-05-21 20:51:34 -0400

    Added some functions for serverless db that constantly connect to db.

--- src/Demo.AzFunctions/Function1.cs
+++ src/Demo.AzFunctions/Function1.cs
@@ -7,12 +7,30 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using System.Data.SqlClient;
+using Dapper;
+using System.Data;
+using Microsoft.Extensions.Configuration;
 
 namespace Demo.AzFunctions
 {
     public static class Function1
     {
-        [FunctionName("Function1")]
+        private static IConfiguration Configuration { set; get; }
+
+        static Function1()
+        {
+            var builder = new ConfigurationBuilder();
+            var config = builder
+            .SetBasePath(Environment.CurrentDirectory)
+            .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+            .AddEnvironmentVariables()
+            .Build();
+
+            Configuration = config;
+        }
+
+        [FunctionName("HttpTriggeredFunction")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
@@ -28,6 +46,40 @@ namespace Demo.AzFunctions
             return name != null
                 ? (ActionResult)new OkObjectResult($"Hello, {name}")
                 : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+        }
+
+        [FunctionName("TimerTriggeredFunction1")]
+        public static async Task Handle1(
+            [TimerTrigger("0 */15 * * * *")] TimerInfo timer)
+        {
+            using (var connection = new SqlConnection(Configuration["connection-string"]))
+            {
+                connection.Open();
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandType = CommandType.Text;
+                    command.CommandText = "SELECT 1";
+                    var result = (int)await command.ExecuteScalarAsync().ConfigureAwait(false);
+                }
+            }
+        }
+
+        [FunctionName("HttpTriggeredFunction2")]
+        public static async Task<IActionResult> Handle2(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            using (var connection = new SqlConnection(Configuration["connection-string"]))
+            {
+                var rules = await connection.QueryAsync<WorkflowRule>("select * from workflowRules");
+                return new OkObjectResult(rules);
+            }
+        }
+
+        public class WorkflowRule
+        {
+            public int WorkFlowRuleId { get; set; }
+            public string WorkFlowRules { get; set; }
         }
     }
 }
